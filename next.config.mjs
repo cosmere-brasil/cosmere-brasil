bf4b18bf659def1591254cd344c4d8b0be007ec5
Raavë L. Aires <raave.aires@proton.me>
2024-09-09 00:58:23 -0300

    corrigido arquivo de configuração

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-    images: {
-        formats: ['image/avif', 'image/webp'],
-    },
-}
-
 export default nextConfig;
