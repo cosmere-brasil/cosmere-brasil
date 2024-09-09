f7468515dba303f07a43d6e28dfb6ecb1c9faf85
Raavë L. Aires <raave.aires@proton.me>
2024-09-09 03:44:07 -0300

    tipo corrigido

--- app/components/UniverseCard/UniverseCard.tsx
+++ app/components/UniverseCard/UniverseCard.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 
 interface Props {
     enlace: string,
-    imagem: React.ReactNode,
+    imagem: string | StaticImageData,
     titulo: string,
     descricao: string
 }
